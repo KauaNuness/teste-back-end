3f8ef56566ba27a2469d53fa872fe73a532bc70d
kauanuness <kaua.souza@sptech.school>
2026-06-16 09:39:44 -0300

    feat: create user entity

---
+++ teste-back-end-kaua/src/main/java/com/kaua/testebackend/entity/User.java
@@ -0,0 +1,26 @@
+package com.kaua.testebackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String phone;
+    
+}
