121848bbff966d40e595db60ce9a530b835b140d
kauanuness <kaua.souza@sptech.school>
2026-06-16 09:15:25 -0300

    chore: initialize spring boot project

---
+++ teste-back-end-kaua/src/main/java/com/kaua/testebackend/TesteBackEndKauaApplication.java
@@ -0,0 +1,13 @@
+package com.kaua.testebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBackEndKauaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteBackEndKauaApplication.class, args);
+	}
+
+}
---
+++ teste-back-end-kaua/src/test/java/com/kaua/testebackend/TesteBackEndKauaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaua.testebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteBackEndKauaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
